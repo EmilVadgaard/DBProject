f9a4a45fcb573b15905fede410e0fa30ba184b41
Emil Nielsen <emiln17@student.sdu.dk>
2024-03-03 11:28:15 +0100

    ændringer på Supplier skema, og mere data indsat

--- insert.sql
+++ insert.sql
@@ -43,24 +43,24 @@ VALUES
 
 INSERT INTO Supplier (SupplierVAT, SupplierName, Address, Phone, Email)
 VALUES 
-('DK10000001', 'Elgiganten', 'Butiksvej 23 2620 Albertslund', '+4560510013', 'El@giganten.dk'),
-('DK10000002', 'Power', 'Butiksvej 21 2620 Albertslund', '+4553343035', 'kontor@power.dk'),
+('DK10000001', 'ElLeverandøren', 'Butiksvej 23 2620 Albertslund', '+4560510013', 'El@leverandøren.dk'),
+('DK10000002', 'PowerSquared', 'Butiksvej 21 2620 Albertslund', '+4553343035', 'kontor@power2.dk'),
 ('DE123456789', 'Media Markt', 'TysklandStrasse 1 13187 Pankow', '+499234564381', 'Desk@MedMar.com'),
 ('NL123456789B01', 'Amazon', null, 'Langegracht 69-H 2312 NW Leiden', '+31-24-3611111', 'office@Amazon.nl'),
 ('GB123456789', 'Amazon', null, '27 Fox Lane RG17 9ZS Bockhampton', '+442071234567', 'office@Amazon.co.uk');
 
 INSERT INTO Supply (InvoiceID, SupplierVAT, Date)
 VALUES
-(301, 'DK10000001', 2023-03-31),
-(302, 'DK10000002', 2023-06-12),
-(303, 'DE123456789', 2023-11-10),
-(304, 'NL123456789B01', 2023-08-05),
-(305, 'GB123456789', 2024-01-22),
-(306, 'DE123456789', 2023-04-27),
-(307, 'NL123456789B01', 2023-12-01),
-(308, 'DK10000002', 2024-02-29),
-(309, 'NL123456789B01', 2023-05-10),
-(310, 'DK10000001', 2023-09-30);
+(301, 'DK10000001', 2022-03-31),
+(302, 'DK10000002', 2022-06-12),
+(303, 'DE123456789', 2022-11-10),
+(304, 'NL123456789B01', 2022-08-05),
+(305, 'GB123456789', 2023-01-22),
+(306, 'DE123456789', 2022-04-27),
+(307, 'NL123456789B01', 2022-12-01),
+(308, 'DK10000002', 2022-02-29),
+(309, 'NL123456789B01', 2022-05-10),
+(310, 'DK10000001', 2022-09-30);
 
 INSERT INTO Product_Supply (InvoiceID, ProductID, Quantity, Value)
 VALUES
@@ -90,16 +90,16 @@ VALUES
 
 INSERT INTO Sale_of_Product (SaleID, ProductID, Quantity, Value)
 VALUES
-(401),
-(402),
-(403),
-(404),
-(405),
-(406),
-(407),
-(408),
-(409),
-(410);
+(401, 101, 166, 11000),
+(402, 102, 49, 6000),
+(403, 103, 10, 8000),
+(404, 103, 3, 10000),
+(405, 105, 9, 800),
+(406, 104, 3, 3499),
+(407, 104, 110, 2200),
+(408, 106, 12, 2200),
+(409, 106, 1, 3200),
+(410, 107, 45, 4500);
 
 INSERT INTO Product_Return (SaleID, ProductID, Date, Quantity)
 VALUES
